17e65c905569f7943fb0b13d1446ff608c9e6180
Anonimeishon <sebastianmperea@gmail.com>
2026-04-20 14:42:09 -0500

    fix: adjust trainer movement speed to improve gameplay responsiveness

--- js/constants/player.js
+++ js/constants/player.js
@@ -1,2 +1,2 @@
 export const TRAINER_SPRITE_SIZE = 64;
-export const TRAINER_MOVEMENT_SPEED_MS = 400; // Time to move one tile (in milliseconds)
+export const TRAINER_MOVEMENT_SPEED_MS = 300; // Time to move one tile (in milliseconds)
