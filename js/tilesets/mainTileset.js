81266ad68e0c72d1614ff5f7b706b60a469961e4
Anonimeishon <sebastianmperea@gmail.com>
2026-04-23 11:37:15 -0500

    fix: streamline solid tile IDs and simplify loadMainTileset image loading

--- js/tilesets/mainTileset.js
+++ js/tilesets/mainTileset.js
@@ -8,19 +8,15 @@ export const MAIN_TILESET_TILES_PER_SHEET_ROW = 16;
 
 export const MAIN_TILESET_SOLID_TILE_IDS = new Set([
   //-2 would be a black tile
-  -2, 0, 2, 3, 5, 6, 7, 8, 9, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 30,
-  31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-  50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68,
-  69, 70, 71,
+  -2, 0, 2, 3, 5, 6, 7, 8, 9, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 30, 31, 32, 33, 34, 35,
+  36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+  60, 61, 62, 63, 64, 65, 66, 67, 68,
 ]);
 
 export const mainTileBuilder = createTileBuilder(MAIN_TILESET_NAME);
 
 export const loadMainTileset = async () => {
-  await sharedLoader.loadImage(
-    MAIN_TILESET_NAME,
-    `${ASSETS_BASE}tilesets/mainTileset.png`,
-  );
+  await sharedLoader.loadImage(MAIN_TILESET_NAME, `${ASSETS_BASE}tilesets/mainTileset.png`);
 
   const image = sharedLoader.get(MAIN_TILESET_NAME);
 
